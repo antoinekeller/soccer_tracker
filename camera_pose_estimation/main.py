baa0fc5123634fb5074b45c8bb3439d4e87d9756
Antoine Keller <ake@outsight.tech>
2022-10-20 17:58:04 +0200

    Add more files in camera_pose_estimation

--- camera_pose_estimation/main.py
+++ camera_pose_estimation/main.py
@@ -6,23 +6,12 @@ from argparse import ArgumentParser
 from pitch_tracker.common import intersect
 
 from pitch_tracker.main import find_key_points
+from projector import (
+    draw_pitch_lines,
+    project_to_screen,
+)
 
-
-# Points in world with origin au centre, x a droite, y vers le fond, z en haut
-central_world = [0, 0, 0]
-right_circle_world = [9.15, 0, 0]
-left_circle_world = [-9.15, 0, 0]
-behind_circle_world = [0, 0, 9.15]
-front_circle_world = [0, 0, -9.15]
-front_middle_line_world = [0, 0, -34]
-back_middle_line_world = [0, 0, 34]
-test_point_world = [30, 0, 34]
-help_point_world = [-30, 0, 34]
-
-corner_back_left_world = [-52.5, 0, 34]
-corner_front_left_world = [-52.5, 0, -34]
-corner_back_right_world = [52.5, 0, 34]
-corner_front_right_world = [52.5, 0, -34]
+from points_in_world import *
 
 
 def find_extrinsic_intrinsic_matrices(img, fx, guess_rot, guess_trans, key_points):
@@ -140,130 +129,6 @@ def find_extrinsic_intrinsic_matrices(img, fx, guess_rot, guess_trans, key_point
     return to_device_from_world, K, rotation_vector, translation_vector
 
 
-def project_to_screen(K, to_device_from_world, point_in_world):
-    homog = np.ones((4, 1))
-    homog[0:3, 0] = point_in_world
-    point_in_device = np.dot(to_device_from_world, homog)
-    point_in_device_divided_depth = (point_in_device / point_in_device[2, 0])[0:3]
-    # print(point_in_device_divided_depth)
-    point_projected = np.dot(K, point_in_device_divided_depth)
-    return [int(point_projected[0]), int(point_projected[1])]
-
-
-def draw_central_circle(K, to_device_from_world, img):
-    # Draw circle
-    res = 25
-    circle_points_projected = np.zeros((res, 2), dtype=np.int32)
-    for i in range(res):
-        angle = i / res * np.pi * 2
-        circle_points_world = np.array([np.cos(angle), 0, np.sin(angle)]) * 9.15
-        circle_points_projected[i] = project_to_screen(
-            K, to_device_from_world, circle_points_world
-        )
-
-    img = cv2.polylines(
-        img, [circle_points_projected], isClosed=True, color=(0, 165, 255), thickness=3
-    )
-
-    return img
-
-
-def draw_middle_line(K, to_device_from_world, img):
-    # Draw middle line
-    back_middle_line_projected = project_to_screen(
-        K, to_device_from_world, np.array(back_middle_line_world)
-    )
-    front_middle_line_projected = project_to_screen(
-        K, to_device_from_world, np.array(front_middle_line_world)
-    )
-    img = cv2.line(
-        img,
-        back_middle_line_projected,
-        front_middle_line_projected,
-        color=(0, 165, 255),
-        thickness=3,
-    )
-
-    return img
-
-
-def project_to_screen(K, to_device_from_world, point_in_world):
-    homog = np.ones((4, 1))
-    homog[0:3, 0] = point_in_world
-    point_in_device = np.dot(to_device_from_world, homog)
-    point_in_device_divided_depth = (point_in_device / point_in_device[2, 0])[0:3]
-    # print(point_in_device_divided_depth)
-    point_projected = np.dot(K, point_in_device_divided_depth)
-    return [int(point_projected[0]), int(point_projected[1])]
-
-
-def draw_central_circle(K, to_device_from_world, img):
-    # Draw circle
-    res = 25
-    circle_points_projected = np.zeros((res, 2), dtype=np.int32)
-    for i in range(res):
-        angle = i / res * np.pi * 2
-        circle_points_world = np.array([np.cos(angle), 0, np.sin(angle)]) * 9.15
-        circle_points_projected[i] = project_to_screen(
-            K, to_device_from_world, circle_points_world
-        )
-
-    img = cv2.polylines(
-        img, [circle_points_projected], isClosed=True, color=(0, 165, 255), thickness=3
-    )
-
-    return img
-
-
-def draw_lateral_lines(K, to_device_from_world, img):
-    corner_back_left_projected = project_to_screen(
-        K, to_device_from_world, np.array(corner_back_left_world)
-    )
-    corner_front_left_projected = project_to_screen(
-        K, to_device_from_world, np.array(corner_front_left_world)
-    )
-    corner_back_right_projected = project_to_screen(
-        K, to_device_from_world, np.array(corner_back_right_world)
-    )
-    corner_front_right_projected = project_to_screen(
-        K, to_device_from_world, np.array(corner_front_right_world)
-    )
-
-    img = cv2.line(
-        img,
-        corner_front_left_projected,
-        corner_front_right_projected,
-        color=(0, 165, 255),
-        thickness=3,
-    )
-
-    img = cv2.line(
-        img,
-        corner_front_right_projected,
-        corner_back_right_projected,
-        color=(0, 165, 255),
-        thickness=3,
-    )
-
-    img = cv2.line(
-        img,
-        corner_back_right_projected,
-        corner_back_left_projected,
-        color=(0, 165, 255),
-        thickness=3,
-    )
-
-    img = cv2.line(
-        img,
-        corner_back_left_projected,
-        corner_front_left_projected,
-        color=(0, 165, 255),
-        thickness=3,
-    )
-
-    return img
-
-
 def find_closer_point_on_line(point, line):
     rho = line[0]
     theta = line[1]
@@ -339,9 +204,7 @@ def calibrate_from_image(img, guess_fx, guess_rot, guess_trans, debug=False, out
     )
 
     if to_device_from_world is not None:
-        img = draw_central_circle(K, to_device_from_world, img)
-        img = draw_middle_line(K, to_device_from_world, img)
-        img = draw_lateral_lines(K, to_device_from_world, img)
+        img = draw_pitch_lines(K, to_device_from_world, img)
 
     return K, to_device_from_world, guess_rot, guess_trans, img
 
---
+++ camera_pose_estimation/projector.py
@@ -0,0 +1,136 @@
+import numpy as np
+import cv2
+
+from points_in_world import *
+
+
+def project_to_screen(K, to_device_from_world, point_in_world):
+    homog = np.ones((4, 1))
+    homog[0:3, 0] = point_in_world
+    point_in_device = np.dot(to_device_from_world, homog)
+    point_in_device_divided_depth = (point_in_device / point_in_device[2, 0])[0:3]
+    # print(point_in_device_divided_depth)
+    point_projected = np.dot(K, point_in_device_divided_depth)
+    return [int(point_projected[0]), int(point_projected[1])]
+
+
+def draw_central_circle(K, to_device_from_world, img):
+    # Draw circle
+    res = 25
+    circle_points_projected = np.zeros((res, 2), dtype=np.int32)
+    for i in range(res):
+        angle = i / res * np.pi * 2
+        circle_points_world = np.array([np.cos(angle), 0, np.sin(angle)]) * 9.15
+        circle_points_projected[i] = project_to_screen(
+            K, to_device_from_world, circle_points_world
+        )
+
+    img = cv2.polylines(
+        img, [circle_points_projected], isClosed=True, color=(0, 165, 255), thickness=3
+    )
+
+    return img
+
+
+def draw_middle_line(K, to_device_from_world, img):
+    # Draw middle line
+    back_middle_line_projected = project_to_screen(
+        K, to_device_from_world, np.array(back_middle_line_world)
+    )
+    front_middle_line_projected = project_to_screen(
+        K, to_device_from_world, np.array(front_middle_line_world)
+    )
+    img = cv2.line(
+        img,
+        back_middle_line_projected,
+        front_middle_line_projected,
+        color=(0, 165, 255),
+        thickness=3,
+    )
+
+    return img
+
+
+def project_to_screen(K, to_device_from_world, point_in_world):
+    homog = np.ones((4, 1))
+    homog[0:3, 0] = point_in_world
+    point_in_device = np.dot(to_device_from_world, homog)
+    point_in_device_divided_depth = (point_in_device / point_in_device[2, 0])[0:3]
+    # print(point_in_device_divided_depth)
+    point_projected = np.dot(K, point_in_device_divided_depth)
+    return [int(point_projected[0]), int(point_projected[1])]
+
+
+def draw_central_circle(K, to_device_from_world, img):
+    # Draw circle
+    res = 25
+    circle_points_projected = np.zeros((res, 2), dtype=np.int32)
+    for i in range(res):
+        angle = i / res * np.pi * 2
+        circle_points_world = np.array([np.cos(angle), 0, np.sin(angle)]) * 9.15
+        circle_points_projected[i] = project_to_screen(
+            K, to_device_from_world, circle_points_world
+        )
+
+    img = cv2.polylines(
+        img, [circle_points_projected], isClosed=True, color=(0, 165, 255), thickness=3
+    )
+
+    return img
+
+
+def draw_lateral_lines(K, to_device_from_world, img):
+    corner_back_left_projected = project_to_screen(
+        K, to_device_from_world, np.array(corner_back_left_world)
+    )
+    corner_front_left_projected = project_to_screen(
+        K, to_device_from_world, np.array(corner_front_left_world)
+    )
+    corner_back_right_projected = project_to_screen(
+        K, to_device_from_world, np.array(corner_back_right_world)
+    )
+    corner_front_right_projected = project_to_screen(
+        K, to_device_from_world, np.array(corner_front_right_world)
+    )
+
+    img = cv2.line(
+        img,
+        corner_front_left_projected,
+        corner_front_right_projected,
+        color=(0, 165, 255),
+        thickness=3,
+    )
+
+    img = cv2.line(
+        img,
+        corner_front_right_projected,
+        corner_back_right_projected,
+        color=(0, 165, 255),
+        thickness=3,
+    )
+
+    img = cv2.line(
+        img,
+        corner_back_right_projected,
+        corner_back_left_projected,
+        color=(0, 165, 255),
+        thickness=3,
+    )
+
+    img = cv2.line(
+        img,
+        corner_back_left_projected,
+        corner_front_left_projected,
+        color=(0, 165, 255),
+        thickness=3,
+    )
+
+    return img
+
+
+def draw_pitch_lines(K, to_device_from_world, img):
+    img = draw_central_circle(K, to_device_from_world, img)
+    img = draw_middle_line(K, to_device_from_world, img)
+    img = draw_lateral_lines(K, to_device_from_world, img)
+
+    return img
