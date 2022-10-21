be6d67a6acd4fb4fb2217475b46596a665fc2dc3
Antoine Keller <ake@outsight.tech>
2022-10-21 23:19:12 +0200

    Clean main.py

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-import cv2
-import numpy as np
-import os
+"""
+Main script to use the whole algorithms.
+First detect key points on the soccer pitch and find the
+camera intrinsic/extrinsic calibration matrices
+Then use yolov5 detector to locate players and balls.
+Finally reproject those detections to a top view image of the soccer field
+"""
+
 from argparse import ArgumentParser
 from pathlib import Path
 
+import cv2
+import numpy as np
+
 # from common import draw_point, yolobbox2bbox
 from camera_pose_estimation.main import calibrate_from_image
+from team_assigner_with_ball_tracking import basic_ball_tracker, yolobbox2bbox
 
 from team_assigner import DominantColors
 
 
 def from_world_to_field(x, z):
+    """Map world coordinates to the soccer_field.png image"""
+
     center_field = [826, 520]
     scale = (1585 - 68) / 105  # 105 meters
 
@@ -20,6 +31,10 @@ def from_world_to_field(x, z):
 
 
 def unproject_to_ground(K, to_device_from_world, u, v):
+    """
+    Unproject pixel point (u, v) and find its world coordinates,
+    assuming that its altitude is zero
+    """
     # First to K^-1 * (u, v, 1)
     homog_uv = np.ones((3, 1))
     homog_uv[0, 0] = u
@@ -56,19 +71,23 @@ def unproject_to_ground(K, to_device_from_world, u, v):
 
 
 def is_in_field(X, Z):
+    """Check if point (X, Z) is located inside the soccer field boundaries"""
     return -52.5 < X < 52.5 and -34 < Z < 34
 
 
 if __name__ == "__main__":
-    # Default value of our focal length to start with
-    # Dont forget to change this value if you are using different sizes of images
-    guess_fx = 2000
-
     parser = ArgumentParser(
-        description="Main script to key points on a soccer field image"
+        description="Main script to use the whole algorithms + display top view of the soccer pitch"
     )
     parser.add_argument("input", type=str, help="Image path or folder")
     parser.add_argument("labels", type=str, help="Folder of yolov5 detections")
+    parser.add_argument(
+        "--out",
+        action="store_const",
+        const=True,
+        default=False,
+        help="Output detection images with ball tracking",
+    )
     args = parser.parse_args()
 
     if not Path(args.input).exists():
@@ -78,49 +97,28 @@ if __name__ == "__main__":
 
     field = cv2.imread("soccer_field.png")
     field_copy = field.copy()
-    field = cv2.circle(
-        field, from_world_to_field(52.5, 30), radius=3, color=(0, 0, 255), thickness=3
-    )
-    cv2.imshow("field", field)
-    cv2.waitKey(0)
 
-    # Ball position tracker
-    prev_pos = np.zeros(4)
-    ball_positions = np.zeros((750, 4))
-    i = 0
-    for filename in images:
-        filename = Path(filename)
-        yolo_file = Path(args.labels).joinpath(Path(f"{filename.stem}.txt"))
+    ball_positions = basic_ball_tracker(Path(args.labels))
 
-        if not yolo_file.exists():
-            raise FileExistsError
-
-        data = np.loadtxt(yolo_file)
-        ball = data[data[:, 0] == 1]
-        if len(ball) == 0:
-            ball_positions[i, :] = prev_pos
-        else:
-            ball_positions[i, :] = ball[0, 1:5]
-            prev_pos = ball[0, 1:5]
-        i += 1
-
-    j = 0
-
+    # Default value of our focal length to start with
+    # Dont forget to change this value if you are using different sizes of images
+    guess_fx = 2000
     guess_rot = np.array([[0.25, 0, 0]])
     guess_trans = (0, 0, 80)
 
-    for k, filename in enumerate(images):
+    for i, filename in enumerate(images):
         if not filename.exists():
             continue
 
         print(str(filename))
         img = cv2.imread(str(filename))
 
+        # Find intrinsic/extrinsic camera matrices
         K, to_device_from_world, rot, trans, img = calibrate_from_image(
             img, guess_fx, guess_rot, guess_trans
         )
-        # Exp
 
+        # Load yolov5 inference results
         filename = Path(filename)
         yolo_file = Path(args.labels).joinpath(Path(f"{filename.stem}.txt"))
 
@@ -128,34 +126,27 @@ if __name__ == "__main__":
         height = img.shape[0]
         data = np.loadtxt(yolo_file)
 
-        pt1, pt2 = yolobbox2bbox(
-            ball_positions[k, 0],
-            ball_positions[k, 1],
-            ball_positions[k, 2],
-            ball_positions[k, 3],
-            width,
-            height,
-        )
-        print(pt1, pt2)
-        cv2.rectangle(img, pt1, pt2, color=(255, 255, 0), thickness=3)
+        field = field_copy.copy()
 
-        for i in range(data.shape[0]):
-            if data[i, 0] == 1:
+        for k in range(data.shape[0]):
+            # Skip ball with class id 1
+            if data[k, 0] == 1:
                 continue
 
-            if data[i, 3] < 0.01:
+            # Skip detections with ridiculous values
+            if data[k, 3] < 0.01:
                 continue
 
+            # Transform yolo bounding box to opencv convention
             pt1, pt2 = yolobbox2bbox(
-                data[i, 1], data[i, 2], data[i, 3], data[i, 4], width, height
+                data[k, 1], data[k, 2], data[k, 3], data[k, 4], width, height
             )
 
+            # Make a sub image and find shirt color
             sub_img = img[pt1[1] : pt2[1], pt1[0] : pt2[0]]
             dc = DominantColors(sub_img, 2)
             colors = dc.dominant_colors()
             color = (int(colors[0][2]), int(colors[0][1]), int(colors[0][0]))
-
-            cv2.rectangle(img, pt1, pt2, color=color, thickness=3)
 
             if to_device_from_world is None:
                 continue
@@ -167,6 +158,7 @@ if __name__ == "__main__":
             if not is_in_field(X, Z):
                 continue
 
+            # Mark the player with a circle on the soccer pitch
             field = cv2.circle(
                 field,
                 from_world_to_field(X, Z),
@@ -175,19 +167,20 @@ if __name__ == "__main__":
                 thickness=3,
             )
 
-        pt1, pt2 = yolobbox2bbox(
-            ball_positions[j, 0],
-            ball_positions[j, 1],
-            ball_positions[j, 2],
-            ball_positions[j, 3],
+        ball_bl, ball_tr = yolobbox2bbox(
+            ball_positions[i, 0],
+            ball_positions[i, 1],
+            ball_positions[i, 2],
+            ball_positions[i, 3],
             width,
             height,
         )
         if to_device_from_world is not None:
-            foot_point = int((pt1[0] + pt2[0]) / 2)
+            foot_point = int((ball_bl[0] + ball_tr[0]) / 2)
 
-            X, Z = unproject_to_ground(K, to_device_from_world, foot_point, pt2[1])
+            X, Z = unproject_to_ground(K, to_device_from_world, foot_point, ball_tr[1])
 
+            # Mark the ball with green-blue color
             if is_in_field(X, Z):
                 field = cv2.circle(
                     field,
@@ -196,15 +189,24 @@ if __name__ == "__main__":
                     color=(255, 255, 0),
                     thickness=3,
                 )
-        j += 1
 
-        # cv2.rectangle(img, pt1, pt2, color=(255, 255, 0), thickness=3)
-        # cv2.imshow("field", field)
-        # cv2.waitKey(0)
+        # Modify current value of calibration matrices to get benefit
+        # of this computation for next image
+        guess_rot = (
+            rot if to_device_from_world is not None else np.array([[0.25, 0, 0]])
+        )
+        guess_trans = trans if to_device_from_world is not None else (0, 0, 80)
+        guess_fx = K[0, 0]
 
-        # cv2.imwrite(f"top_view/{filename}", field)
+        # Display result
+        if not args.out:
+            cv2.imshow("field", field)
+            cv2.imshow("img", img)
+            k = cv2.waitKey(0)
 
-        cv2.imshow("Test", img)
-        k = cv2.waitKey(0)
-        if k == 27:
-            break
+            if k == 27:
+                break
+        else:
+            out_path = Path("out").joinpath(Path(filename).name)
+            print(f"Writing image to {str(out_path)}")
+            cv2.imwrite(str(out_path), field)
